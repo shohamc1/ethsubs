84bdf8074453c6e2f9fdb5e3ebc0a991e74821c9
Shoham Chakraborty <shhmchk@gmail.com>
2023-03-29 14:25:54 +0800

    broadcast working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use futures::{join, Stream, StreamExt};
+use futures::{future::{Either, self}, StreamExt};
 use jsonrpsee::{
     core::{
         client::{Subscription, SubscriptionClientT},
@@ -6,96 +6,107 @@ use jsonrpsee::{
     rpc_params,
     server::ServerBuilder,
     ws_client::WsClientBuilder,
-    RpcModule, PendingSubscriptionSink, SubscriptionMessage, TrySendError
+    RpcModule, PendingSubscriptionSink, SubscriptionMessage
 };
-use serde::Serialize;
-use std::net::SocketAddr;
-use std::time::Duration;
 use serde_json::Value;
+use std::net::SocketAddr;
 use tokio::sync::broadcast;
-use tokio::time::interval;
-use tokio_stream::wrappers::{BroadcastStream, IntervalStream};
+use tokio_stream::wrappers::BroadcastStream;
+
+const NUM_SUBSCRIPTION_RESPONSES: usize = 5;
 
 #[tokio::main]
-async fn main() {
-    let server = ServerBuilder::default()
-        .build("127.0.0.1:0".parse::<SocketAddr>().unwrap())
-        .await
-        .unwrap();
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::FmtSubscriber::builder()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .try_init()
+        .expect("setting default subscriber failed");
+
+    let addr = run_server().await?;
+    let url = format!("ws://{}", addr);
+
+    let client1 = WsClientBuilder::default().build(&url).await?;
+    let client2 = WsClientBuilder::default().build(&url).await?;
+    let sub1: Subscription<Value> = client1.subscribe("subscribe_hello", rpc_params![], "unsubscribe_hello").await?;
+    let sub2: Subscription<Value> = client2.subscribe("subscribe_hello", rpc_params![], "unsubscribe_hello").await?;
+
+    let fut1 = sub1.take(NUM_SUBSCRIPTION_RESPONSES).for_each(|r| async move { tracing::info!("sub1 rx: {:?}", r) });
+    let fut2 = sub2.take(NUM_SUBSCRIPTION_RESPONSES).for_each(|r| async move { tracing::info!("sub2 rx: {:?}", r) });
+
+    future::join(fut1, fut2).await;
+
+    Ok(())
+}
+
+async fn run_server() -> anyhow::Result<SocketAddr> {
+    // let's configure the server only hold 5 messages in memory.
+    let server = ServerBuilder::default().set_message_buffer_capacity(5).build("127.0.0.1:0").await?;
     let (tx, _rx) = broadcast::channel::<Value>(16);
 
     let mut module = RpcModule::new(tx.clone());
 
+    std::thread::spawn(move || produce_items(tx));
+
     module
-        .register_subscription("sub_ping", "notif_ping", "unsub_ping", |_, mut pending, ctx| async move {
-            let rx = ctx.subscribe();
+        .register_subscription("subscribe_hello", "s_hello", "unsubscribe_hello", |_, pending, tx| async move {
+            let rx = tx.subscribe();
             let stream = BroadcastStream::new(rx);
-            pipe_from_stream_and_drop(pending, stream).await.map_err::<anyhow::Error, _>(Into::into);
+            pipe_from_stream_with_bounded_buffer(pending, stream).await?;
+            Ok(())
         })
         .unwrap();
+    let addr = server.local_addr()?;
+    let handle = server.start(module)?;
 
-    let addr = server.local_addr().unwrap();
-
-    let handle = server.start(module).unwrap();
-
+    // In this example we don't care about doing shutdown so let's it run forever.
+    // You may use the `ServerHandle` to shut it down or manage it yourself.
     tokio::spawn(handle.stopped());
 
-    let url = format!("ws://{}", addr);
-    let client = WsClientBuilder::default().build(&url).await.unwrap();
-
-    let mut ping_sub: Subscription<Value> = client
-        .subscribe("sub_ping", rpc_params!(), "unsub_ping")
-        .await
-        .unwrap();
-
-    tokio::spawn(send_items(tx));
-
-    for _ in 0..10 {
-        let ping = ping_sub.next().await.unwrap().unwrap();
-        println!("ping: {:?}", ping);
-    }
+    Ok(addr)
 }
 
-pub async fn pipe_from_stream_and_drop(
+async fn pipe_from_stream_with_bounded_buffer(
     pending: PendingSubscriptionSink,
-    mut stream: BroadcastStream<Value>
+    stream: BroadcastStream<Value>,
 ) -> Result<(), anyhow::Error> {
-    let mut sink = pending.accept().await?;
+    let sink = pending.accept().await?;
+    let closed = sink.closed();
+
+    futures::pin_mut!(closed, stream);
 
     loop {
-        tokio::select! {
-			_ = sink.closed() => return Err(anyhow::anyhow!("Subscription was closed")),
-			maybe_item = stream.next() => {
-				match maybe_item {
-                    Some(Err(e)) => {
-                        println!("e : {:#?}", e);
-                    },
-					None => return Err(anyhow::anyhow!("Subscription executed successful")),
-					Some(item) => {
-                    let msg = SubscriptionMessage::from_json(&item.unwrap())?;
+        match future::select(closed, stream.next()).await {
+            // subscription closed.
+            Either::Left((_, _)) => break Ok(()),
 
-                    match sink.try_send(msg) {
-                        Ok(_) => (),
-                        Err(TrySendError::Closed(_)) => return Err(anyhow::anyhow!("Subscription executed successful")),
-                        // channel is full, let's be naive an just drop the message.
-                        Err(TrySendError::Full(_)) => (),
-                    }
+            // received new item from the stream.
+            Either::Right((Some(Ok(item)), c)) => {
+                let notif = SubscriptionMessage::from_json(&item)?;
+
+                // NOTE: this will block until there a spot in the queue
+                // and you might want to do something smarter if it's
+                // critical that "the most recent item" must be sent when it is produced.
+                if sink.send(notif).await.is_err() {
+                    break Ok(());
                 }
-                }
+
+                closed = c;
             }
+
+            // Send back back the error.
+            Either::Right((Some(Err(e)), _)) => break Err(e.into()),
+
+            // Stream is closed.
+            Either::Right((None, _)) => break Ok(()),
         }
     }
 }
 
-async fn send_items(tx: broadcast::Sender<Value>) {
-    let data = r#"
-        {
-            "hello": "there"
-        }"#;
+// Naive example that broadcasts the produced values to all active subscribers.
+fn produce_items(tx: broadcast::Sender<Value>) {
+    for _ in 1..=100 {
+        std::thread::sleep(std::time::Duration::from_millis(1));
 
-    for c in 1..=100 {
-        let v = serde_json::from_str(data).unwrap();
-        let _ = tx.send(v);
-        std::thread::sleep(std::time::Duration::from_millis(100));
+        let _ = tx.send(Value::String("asdf".to_owned()));
     }
 }
